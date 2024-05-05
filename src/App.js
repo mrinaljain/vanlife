de10781cedde5f5bb6a1157c25facaf1d08c9567
mrinaljain <jain.mrinal140@gmail.com>
2024-05-05 13:32:40 +0530

    small update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from './pages/About/About';
 import Vans from './pages/Vans/Vans';
 import Home from './pages/Home/Home';
